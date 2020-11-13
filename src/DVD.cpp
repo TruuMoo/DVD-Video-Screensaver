0cd287ce30c4068dbee1963307150dc1f2b05f09
samvader13@gmail.com <samvader13@gmail.com>
2020-11-12 20:01:51 -0600

    Removed useless library modules and cleaned up code

--- src/DVD.cpp
+++ src/DVD.cpp
@@ -1,6 +1,9 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
+/* define this if you're running the file in Visual Studio */
+//#define RUNNING_IN_VS   
+
 #define STARTING_X_POSITION 400
 #define STARTING_Y_POSITION 400
 
@@ -8,11 +11,12 @@
 #define STARTING_X_VELOCITY 5 
 #define STARTING_Y_VELOCITY 5
 
-static constexpr int_fast16_t WINDOW_WIDTH = 800;
+#define LOGO_SIZE sf::Vector2f(300.f, 131.f)
+
+static constexpr int_fast16_t WINDOW_WIDTH = 900;
 static constexpr int_fast16_t WINDOW_HEIGHT = 800;
 static constexpr int FRAMERATE = 60;
-static constexpr float SPEED = 0.3f;
-
+static constexpr float SPEED = 0.4f;
 
 
 sf::Color getRandomColor() {
@@ -28,17 +32,21 @@ int main()
 {
 
     sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "DVD Video");
-    sf::RectangleShape shape(sf::Vector2f(300.f, 131.f));
+    sf::RectangleShape shape(LOGO_SIZE);
 
     sf::Texture texture;
     sf::Sprite sprite;
 
-    if (!texture.loadFromFile(".\\img\\dvdLogo.png"))
-        std::cerr << "ERROR: COULD NOT LOAD IMAGE";
+    #ifndef RUNNING_IN_VS
+        texture.loadFromFile("../img/dvdLogo.png");
+    #else
+        texture.loadFromFile("./img/dvdLogo.png");
+    #endif
+            
 
     sprite.setTexture(texture);
     window.setFramerateLimit(FRAMERATE);
-    window.setVerticalSyncEnabled(true);
+    sprite.setColor(getRandomColor());
 
 
     sprite.setPosition(STARTING_X_POSITION, STARTING_Y_POSITION);
@@ -54,8 +62,7 @@ int main()
         x = (int)(sprite.getPosition().x + (0.5 * (sprite.getGlobalBounds().width) + 1));
         y = (int)(sprite.getPosition().y + (0.5 * (sprite.getGlobalBounds().height)));
 
-        std::cout << x << ", " << y << std::endl;
-        //std::cout << shape.getPosition().x << ", " << shape.getPosition().y << std::endl;
+        //std::cout << x << ", " << y << std::endl;
 
         if (sprite.getPosition().y <= 0 || (sprite.getGlobalBounds().top + sprite.getGlobalBounds().height) >= WINDOW_HEIGHT) {
             vy = -(vy); sprite.setColor(getRandomColor());
